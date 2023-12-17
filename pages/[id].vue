1aee26a23ce33ff9d456e2f53b25543794da1beb
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-12-17 15:40:32 +0700

    add: data content fetch

--- pages/[id].vue
+++ pages/[id].vue
@@ -4,17 +4,17 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 col-sm-12">
-                    <img src="/bg.jpg" class="img-thumbnail w-100" alt="...">
+                    <img v-if="content" :src="content.pictures[0].url" class="img-thumbnail w-100" alt="...">
                 </div>
                 <div class="col-lg-6 col-sm-12">
-                    <h3>{{ id }}</h3>
+                    <h3>{{ content?.title ?? '-' }}</h3>
                     <div class="d-flex justfy-content-start gap-2 py-2">
                         <button class="btn btn-primary">Gambar</button>
                         <button class="btn btn-primary">Gambar</button>
                         <button class="btn btn-primary">Gambar</button>
                     </div>
                     <p>
-                        Apakah Anda mencari gambar tentang Gambar Bunga Mawar Buat Kekasih? Terdapat 53 Koleksi Gambar berkaitan dengan Gambar Bunga Mawar Buat Kekasih, File yang di unggah terdiri dari berbagai macam ukuran dan cocok digunakan untuk Desktop PC, Tablet, Ipad, Iphone, Android dan Lainnya. Silahkan lihat koleksi gambar lainnya dibawah ini untuk menemukan gambar yang sesuai dengan kebutuhan anda.
+                        {{ content?.meta ?? '-' }}
                     </p>
                     <div class="mt-3">
                         <div class="card bg-primary p-2 text-white">
@@ -34,28 +34,35 @@
     </section>
     <section>
         <div class="container">
-            <h4>Koleski {{ id }}</h4>
+            <h4 v-if="content">Koleksi</h4>
             <div class="row">
-                <div class="col-lg-3">
-            <div class="card">
-                <img src="/halal.jpeg" class="card-img-top" alt="images">
-                <div class="card-body d-block d-flex justify-content-between">
-                  <h3 class="fs-5">Some quick </h3>
-                  <div class="border-none btn btn-light btn-sm rounded-5 items-align-center" type="button"><i class="bi bi-eye"></i></div>
-                </div>
-                <div class="py-3 px-3">
-                  <a href="#" class="text-start fw-normal text-decoration-none">Lihat Detail</a>
-                </div>
-              </div>
+                <div v-if="content" v-for="(picture, index) in content.pictures" :key="picture.id" class="col-lg-3">
+                    <div class="card">
+                        <img :src="picture.url" class="card-img-top" alt="images">
+                        <div class="card-body d-block d-flex justify-content-between">
+                        <h3 class="fs-5">Koleksi Gambar No. {{ index + 1 }} </h3>
+                        <div class="border-none btn btn-light btn-sm rounded-5 items-align-center" type="button"><i class="bi bi-eye"></i></div>
+                        </div>
+                        <div class="py-3 px-3">
+                        <a href="#" class="text-start fw-normal text-decoration-none">Lihat Detail</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
   </div>
 </template>
+
 <script setup>
-    const { id } = useRoute().params
+import axios from 'axios'
+
+const { data: content } = useAsyncData('content', async () => {
+    try {
+        const response = await axios.get(`http://localhost:8000/api/contents/${useRoute().params.id}`)
+        return response.data.data.content;
+    } catch(error) {
+        return null;
+    }
+})
 </script>
-<style lang="">
-    
-</style>
