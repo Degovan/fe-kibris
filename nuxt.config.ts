803179321a1fb5ea2faa52ebd35da510c4dcbad6
sahrullahh <mohammadsahrullah894@gmail.com>
2023-12-19 15:35:32 +0700

    fixing: upload vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            title: "Nuxt 3",
-            titleTemplate: "%s | Nuxt 3",
-            meta: [
-                {
-                    name: "viewport",
-                    content: "width=device-width, initial-scale=1",
-                },
-                { hid: "description", name: "description", content: "Nuxt 3" },
-                { charset: "utf-8" },
-            ],
-            link: [
-                {
-                    rel: "stylesheet",
-                    integrity:
-                        "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN",
-                    crossorigin: "anonymous",
-                    href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-                },
-            ],
-            script: [
-                {
-                    src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
-                    integrity: "sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL",
-                    crossorigin: "anonymous",
-                },
-            ],
+  app: {
+    head: {
+      title: "Nuxt 3",
+      titleTemplate: "%s | Nuxt 3",
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
         },
+        { hid: "description", name: "description", content: "Nuxt 3" },
+        { charset: "utf-8" },
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          integrity:
+            "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN",
+          crossorigin: "anonymous",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
+        },
+      ],
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+          integrity:
+            "sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL",
+          crossorigin: "anonymous",
+        },
+      ],
     },
-    devtools: { enabled: true },
-    modules: ["nuxt-icon"],
+  },
+  build: {
+    transpile: ["applicationinsights"],
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/"],
+    },
+  },
+
+  ssr: false,
+  devtools: { enabled: false },
+  modules: ["nuxt-icon"],
 });
