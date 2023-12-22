be479ae91e61efa8c0b35fc59de032eec5db0b19
destroylord <masapin68@gmail.com>
2023-12-22 14:03:36 +0700

    update card

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,15 @@
           <div class="small-border"></div>
           <div class="row">
             <div class="col-lg-3" v-for="item in picture" :key="item.id">
-              <div class="card">
+            <div class="hover hover-4 text-white rounded"><img src="https://bootstrapious.com/i/snippets/sn-img-hover/hoverSet-7.jpg" alt="">
+              <div class="hover-overlay"></div>
+              <div class="hover-4-content">
+                <h3 class="hover-4-title text-uppercase font-weight-bold mb-0"><span class="font-weight-light">Image </span>Caption</h3>
+                <p class="hover-4-description text-uppercase mb-0 small">Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt</p>
+              </div>
+            </div>
+             
+              <!-- <div class="card">
                 <img src="/halal.jpeg" class="card-img-top" alt="images">
                 <div class="card-body d-block d-flex justify-content-between">
                   <h3 class="fs-5"> {{ item.data }} </h3>
@@ -23,7 +31,7 @@
                 <div class="py-3 px-3">
                   <a href="#" class="text-start fw-normal text-decoration-none">Lihat Detail</a>
                 </div>
-              </div>
+              </div> -->
             </div>
           </div>
           <div class="d-flex justify-content-center align-items-center mt-5">
